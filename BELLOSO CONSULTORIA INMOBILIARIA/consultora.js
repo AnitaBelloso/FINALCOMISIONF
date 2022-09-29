9f0e7ab9ae7e7a2da85e490eb57c2c011bcc0dae
Anita Belloso <alison_belloso@hotmail.com>
2022-09-29 19:22:48 -0300

    subiendo proyecto final

---
+++ BELLOSO CONSULTORIA INMOBILIARIA/consultora.js
@@ -0,0 +1,13 @@
+let alquiler= prompt ("Estás alquilando una propiedad con destino a vivienda?");
+let contrato = prompt ("Indique el año en el cual contrató:");
+if(alquiler= "si","SI","Si");
+if (contrato >= (2020)) {
+alert("Podemos asesorarte sobre las modificaciones en la Ley de Contrato de Alquiler.");
+}
+
+
+if (alquiler="no","No","NO");
+
+else {
+    alert("Estamos para asesorarte en tu próximo contrato de alquier. No dudes en consultarnos!");
+}
